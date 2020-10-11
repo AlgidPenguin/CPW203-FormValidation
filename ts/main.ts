ba307dfefedff582f83e3fec3550874a7ead04ab
unknown <daniel_r129@hotmail.com>
2020-10-11 15:11:25 -0700

    Created seperate function for validation

--- ts/main.ts
+++ ts/main.ts
@@ -5,5 +5,26 @@ window.onload = function() {
 }
 
 function main():void {
-    alert("Reg button was clicked");
+    isTextPresent("first-name", "First name is required");
+    isTextPresent("last-name", "Last name is required");
+}
+
+/**
+ * Returns true if the textbox with the given id
+ * has text inside, false otherwise
+ * @param id The id of the <input type="text"> to validate
+ * @param errMsg Desired message to display in the sibling
+ * span of the textbox
+ */
+function isTextPresent(id:string, errMsg:string):boolean {
+    let txtBox = <HTMLInputElement>document.getElementById(id);
+
+    let txtBoxValue = txtBox.value;
+
+    if (txtBoxValue == "") {
+        let errorSpan = <HTMLElement>txtBox.nextElementSibling;
+        errorSpan.innerText = errMsg;
+        return false;
+    }
+    return true;
 }
